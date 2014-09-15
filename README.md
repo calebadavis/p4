9eac041193ca1c729c123a6a4082192969fc7161
Caleb Davis <caleb.davis@gmail.com>
2014-09-14 17:34:46 -0700

    Initial .gitignore and placeholder index+README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project 4, Caleb Davis, CSCI-E15 Fall 2014
+
+## Live URL
+<http://p4.prowlers.com>
+
+## Description
+
+
+
---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>P4 Placeholder Page</title>
+	<meta charset='utf-8'/>
+		
+</head>
+<body>
+
+	<?php 
+	echo 'Hello World! You are visiting ';
+	echo $_SERVER['SERVER_NAME']; 
+	?>
+	
+</body>
+</html>
